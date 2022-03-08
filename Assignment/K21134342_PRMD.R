f4f11f4b39e8fe75534f48de38186447cf05d6f0
Changhyun Kim <movementfor@gmail.com>
2022-03-08 23:47:03 +0000

    Doing assignment

--- Assignment/K21134342_PRMD.R
+++ Assignment/K21134342_PRMD.R
@@ -25,3 +25,28 @@ dim(data)
 
 # About 1000 observations were removed
 summary(data)
+
+# 1.2 Model 1: Logistic regression
+
+# install.packages("pmsampsize")
+library(pmsampsize)
+
+# Prevalence
+summary(data$d.unfav)
+# 0   1
+# 653 458 => 0.4122412
+prevalence <- 458 / (653 + 458)
+
+# Power analysis
+pmsampsize(type="b", rsquared=0.2, parameters = 20, shrinkage = 0.9, prevalence = prevalence, seed=123)
+
+nrow(data) # 1111
+nrow(data[data$trial == "Tirilazad US",]) # 545
+
+# Cite the package
+citation("pmsampsize")
+
+# The maximum number of params that used for analysis of both dataset was 13 suggested by pmsampsize
+pmsampsize(type="b", rsquared=0.2, parameters = 13, shrinkage = 0.9, prevalence = prevalence, seed=123)
+
+train <- data[data$trial == "Tirilazad US"]
